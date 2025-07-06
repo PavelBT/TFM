ce636793d52102ca6f968d21029b069b72886833
PavelBT <69017873+PavelBT@users.noreply.github.com>
2025-07-05 23:08:07 -0600

    Mejora de promt y ejemplos json

--- app/services/ocr/gemini/gemini_service.py
+++ app/services/ocr/gemini/gemini_service.py
@@ -13,13 +13,6 @@ try:
     import google.generativeai as genai
     from google.generativeai.types import HarmCategory, HarmBlockThreshold
 
-    # Configure the Google Generative AI library
-    genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-    models = genai.list_models()
-
-    for m in models:
-        print(m.name, m.supported_generation_methods)
-
 except Exception:  # pragma: no cover - optional dependency may not be installed
     genai = None
 
@@ -33,11 +26,94 @@ class GeminiOCRService(OCRService):
             or os.getenv("GOOGLE_AI_STUDIO_API_KEY", "")
         )
         self.model_name = model_name or os.getenv("GEMINI_MODEL", "gemini-2.0-pro-exp-02-05")
-        self.prompt = "Analyze the given document and carefully extract the information,\
-            the language of the document is Spanish, the output format is JSON in plaintext,\
-            you should identify the form_name only credito_personal, credito_hipotecario, solicitud_credito or desconocido,\
-            containing the key <form_name> in first position. all text is in Spanish. Always the otput is a JSON with structured data by form section."
-        
+        # self.prompt = "Analyze the given document and carefully extract the information,\
+        #     the language of the document is Spanish, the output format is JSON in plaintext,\
+        #     you should identify the form_name only credito_personal, credito_hipotecario, solicitud_credito or desconocido,\
+        #     containing the key <form_name> in first position. all text is in Spanish. Always the otput is a JSON with structured data by form section."
+        self.prompt = '''Extrae la información clave del siguiente documento en español y devuélvela exclusivamente en formato JSON estructurado, siguiendo exactamente las secciones y alias indicados. 
+        Si algún campo no aparece en el documento, plasmalo en el mismo formato siempre dentro de una seccion existente (no incluyas claves vacías ni valores `null`).
+
+Puden ser 2 tipos de documentos: "credito_personal" o "credito_hipotecario", si no es ninguno de estos, devuelve el campo "form_name" con el valor "desconocido".
+Todas las fechas deben ser devueltas como una sola cadena en el formato "dd/mm/aaaa". Por ejemplo:
+
+"fecha_nacimiento": "01/01/1990"
+
+La estructura deseada es (ejemkplo para "credito_personal"):
+
+{
+  "form_name": "credito_personal",
+  "datos_solicitud": {
+    "fecha_solicitud": "dd/mm/aaaa",
+    "folio": "",
+    "cliente_id": "",
+    "ejecutivo_nombre": "",
+    "numero_nomina": "",
+    "zona": ""
+  },
+  "informacion_credito": {
+    "monto_solicitado": "",
+    "plazo_meses": ""
+  },
+  "datos_personales": {
+    "nombre": "",
+    "apellido_paterno": "",
+    "apellido_materno": "",
+    "fecha_nacimiento": "dd/mm/aaaa",
+    "pais_nacimiento": "",
+    "nacionalidad": "",
+    "genero": "",
+    "tipo_solicitud": "",
+    "rfc": "",
+    "curp": "",
+    "grado_estudios": "",
+    "telefono_casa": "",
+    "telefono_celular": "",
+    "email": "",
+    "tipo_identificacion": "",
+    "folio_identificacion": "",
+    "estado_civil": "",
+    "regimen_matrimonial": ""
+  },
+  "domicilio": {
+    "tipo_propiedad": "",
+    "direccion": "",
+    "colonia": "",
+    "municipio": "",
+    "ciudad": "",
+    "estado": "",
+    "pais": "",
+    "cp": "",
+    "anios_residencia": "",
+    "domicilio_anterior": ""
+  },
+  "empleo": {
+    "empresa": "",
+    "giro_empresa": "",
+    "ocupacion": "",
+    "puesto": "",
+    "sueldo_mensual": "",
+    "otros_ingresos": "",
+    "fuente_otros_ingresos": "",
+    "jefe_nombre_puesto": "",
+    "telefono_oficina": "",
+    "telefono_oficina_alt": "",
+    "direccion_laboral": "",
+    "empleo_anterior": ""
+  },
+  "referencias_personales": [
+    {
+      "nombre_completo": "",
+      "telefono": "",
+      "horario_llamada": "",
+      "parentesco": ""
+    }
+  ]
+}
+
+para el caso de credito_hipotecario, deberas agregar la secciones: datos_inmueble, informacion_vendedor y servicios_adicionales.  
+
+Responde exclusivamente con un JSON válido. No expliques nada. Usa el idioma y formato textual original del documento.'''
+
         if genai:
             
             genai.configure(api_key=self.api_key)
